0f9c420e177d90562bd1cf316e6a234390553c89
zerob13 <zerob13@gmail.com>
2014-09-16 13:28:30 +0800

    finish first http server task for home.html

--- app/src/main/java/in/zerob13/android/wifialbum/httpserver/WebServerService.java
+++ app/src/main/java/in/zerob13/android/wifialbum/httpserver/WebServerService.java
@@ -1,7 +1,9 @@
 package in.zerob13.android.wifialbum.httpserver;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -10,13 +12,16 @@ import android.util.Log;
  */
 public class WebServerService extends Service {
     private WebServer server = null;
+    private WebServerTask mServerTask = null;
 
     @Override
     public void onCreate() {
         Log.i("HTTPSERVICE", "Creating and starting httpService");
         super.onCreate();
-        server = new WebServer(this);
-        server.startServer();
+        mServerTask = new WebServerTask(this);
+        mServerTask.execute();
+
+
     }
 
     @Override
@@ -30,5 +35,23 @@ public class WebServerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+
+    private class WebServerTask extends AsyncTask {
+
+        private Context mContext;
+        public WebServerTask(Context aContext){
+            mContext=aContext;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            server = new WebServer(mContext);
+            server.startServer();
+            return null;
+        }
+
+
     }
 }
